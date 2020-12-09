45e0c5ade2faa56bf11de7c0900104d319149612
mahak4983 <mahak4983@gmail.com>
2020-12-09 13:32:36 +0530

    add: Product Dimensions Form

--- src/components/Forms/Checkout.js
+++ src/components/Forms/Checkout.js
@@ -5,6 +5,7 @@ import {
     Button,
 } from '@material-ui/core';
 import BuyerDetails from './BuyerDetails';
+import ProductDimensions from './ProductDimensions';
 
 
 
@@ -24,15 +25,17 @@ const useStyles = makeStyles({
         marginBottom: 0
     }
 });
+ 
+const steps = 3;
 
 function getStepContent(step) {
     switch (step) {
         case 0:
             return <BuyerDetails />;
         case 1:
-            // return <PaymentForm />;
+             return <ProductDimensions/>
         case 2:
-            // return <Review />;
+             return <div>Henlo</div>;
         default:
             throw new Error('Unknown step');
     }
@@ -46,158 +49,12 @@ export default function SimpleCard() {
     const handleNextClick = () => {
         setactiveStep(activeStep + 1);
     }
+    const handleBackClick = () => {
+        setactiveStep(activeStep - 1);
+    }
 
     return (
         <Card className={classes.root}>
-            {/* <CardContent style={{ padding: 0 }}>
-                <Typography className={classes.title} gutterBottom style={{ backgroundColor: '#66bb6a' }}>
-                    Buyers Details
-        </Typography>
-                <form>
-                    <Typography className={classes.formHeadings}>Buyer details</Typography>
-                    <Grid container spacing={3} style={{ padding: 50, paddingTop: 10, paddingBottom: 20 }}>
-                        <Grid item xs={12} sm={3}>
-                            <TextField
-                                required
-                                id="Name"
-                                name="Name"
-                                label="Name"
-                                fullWidth
-                                autoComplete="given-name"
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={3}>
-                            <TextField
-                                required
-                                type="number"
-                                id="Phone"
-                                name="Phone"
-                                label="Phone Number"
-                                fullWidth
-                                autoComplete="Phone"
-                                InputProps={{
-                                    startAdornment: <InputAdornment position="start">+91</InputAdornment>,
-                                }}
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={3}>
-                            <TextField
-                                type="number"
-                                id="AltPhone"
-                                name="AltPhone"
-                                label="Alternate Phone Number"
-                                fullWidth
-                                autoComplete="Phone"
-                                InputProps={{
-                                    startAdornment: <InputAdornment position="start">+91</InputAdornment>,
-                                }}
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={3}>
-                            <TextField
-                                required
-                                type="email"
-                                id="email"
-                                name="email"
-                                label="Email"
-                                fullWidth
-                                autoComplete="email"
-                            />
-                        </Grid>
-                    </Grid>
-                    <Divider />
-                    <Typography className={classes.formHeadings} >Buyer Address</Typography>
-
-                    <Grid container spacing={3} style={{ padding: 50, paddingTop: 10 }}>
-
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                required
-                                id="address1"
-                                name="address1"
-                                label="Address line 1"
-                                fullWidth
-                                autoComplete="shipping address-line1"
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                id="address2"
-                                name="address2"
-                                label="Address line 2"
-                                fullWidth
-                                autoComplete="shipping address-line2"
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={3}>
-                            <TextField
-                                required
-                                id="city"
-                                name="city"
-                                label="City"
-                                fullWidth
-                                autoComplete="shipping address-level2"
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={3}>
-                            <TextField required id="state" name="state" label="State/Province/Region" fullWidth />
-                        </Grid>
-                        <Grid item xs={12} sm={3}>
-                            <TextField
-                                required
-                                type="number"
-                                id="zip"
-                                name="zip"
-                                label="Zip / Postal code"
-                                fullWidth
-                                autoComplete="shipping postal-code"
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={3}>
-                            <TextField
-                                required
-                                id="country"
-                                name="country"
-                                label="Country"
-                                fullWidth
-                                autoComplete="shipping country"
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                id="company name"
-                                name="company name"
-                                label="Company Name (Optional)"
-                                fullWidth
-                                autoComplete="shipping company name"
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <FormControlLabel
-                                control={<Checkbox color="secondary" name="saveAddress" value="yes" />}
-                                label="Use this address for payment details"
-                            />
-                        </Grid>
-                    </Grid>
-                    <div
-                        style={{
-                            display: 'flex',
-                            flexDirection: 'row',
-                            justifyContent: 'flex-end',
-                            margin: 20
-                        }}
-                    >
-
-                        <Button
-                            variant="contained"
-                            color="primary"
-                        >
-                            Next
-              </Button>
-                    </div>
-                </form>
-            <BuyerDetails />
-            </CardContent> */}
             {getStepContent(activeStep)}
             <div
                 style={{
@@ -207,7 +64,19 @@ export default function SimpleCard() {
                     margin: 20
                 }}
             >
-
+                {activeStep !== 0 && (
+                    
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={handleBackClick}
+                    style={{marginRight:'5px'}}
+                >
+                    Back
+              </Button>
+                )}
+                {activeStep !== 2 && (
+                    
                 <Button
                     variant="contained"
                     color="primary"
@@ -215,6 +84,17 @@ export default function SimpleCard() {
                 >
                     Next
               </Button>
+                )}
+                {/* TODO add Orderhandler */}
+                {activeStep === 2&&(
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        // onClick={handleNextClick}
+                    >
+                        Order
+                    </Button>
+                )}
             </div>
         </Card>
     );
---
+++ src/components/Forms/ProductDimensions.js
@@ -0,0 +1,150 @@
+import React, { useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import {
+    TextField,
+    Grid,
+    FormControlLabel,
+    Checkbox,
+    Divider,
+    InputAdornment,
+    FormControl,
+    Radio,
+    RadioGroup,
+    FormLabel,
+} from '@material-ui/core'
+const useStyles = makeStyles(theme => ({
+    root: {
+        // minWidth: 275,
+    },
+    title: {
+        fontSize: 20,
+        height: 50,
+        padding: 10,
+        paddingLeft: 55,
+        color: 'white'
+    },
+    form: {
+        paddingLeft: '10%',
+        paddingRight: '10%',
+        [theme.breakpoints.down('sm')]: {
+            padding:'0%',
+        },
+    
+    },
+    radioButton: {
+        marginTop: '5%',
+        [theme.breakpoints.down('sm')]: {
+            margin: '10%',
+        },
+    },
+    
+    formHeadings: {
+        margin: 20,
+        marginBottom: 0
+    }
+}));
+
+const ProductDimensions = () => {
+    const classes = useStyles();
+
+    //State Variables for form fields
+
+    // const [name, setName] = useState('');
+    // const [phoneNumber, setphoneNumber] = useState(0);
+    // const [weightNumber, setweightNumber] = useState(0);
+    // const [email, setemail] = useState('');
+    // const [add1, setadd1] = useState('');
+    // const [add2, setadd2] = useState('');
+    // const [pin, setpin] = useState(0);
+    // const [length, setlength] = useState('');
+    // const [state, setstate] = useState('');
+    // const [country, setcountry] = useState('');
+    // const [companyName, setcompanyName] = useState('');
+
+
+    return (
+        <CardContent style={{ padding: 0 }}>
+            <Typography className={classes.title} gutterBottom style={{ backgroundColor: '#66bb6a' }}>
+                Product Details
+        </Typography>
+            <form className={classes.form}>
+                
+                <Grid container spacing={3} style={{ padding: 50, paddingTop: 10, paddingBottom: 20 }}>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            required
+                            type="number"
+                            id="units"
+                            name="units"
+                            label="No of Units"
+                            fullWidth
+                            autoComplete="units"
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            type="number"
+                            id="weight"
+                            name="weight"
+                            label="Weight"
+                            fullWidth
+                            autoComplete="Weight"
+                            InputProps={{
+                                endAdornment: <InputAdornment position="end">Kg</InputAdornment>,
+                            }}
+
+                        />
+                    </Grid>
+                </Grid>
+                <Divider />
+                <FormControl component="fieldset" className={classes.radioButton}>
+                    <FormLabel component="legend">Select Unit</FormLabel>
+                    {/* TODO add onChange and value Prop to RadioGroup */}
+                    <RadioGroup row aria-label="unit" name="unit1">
+                        <FormControlLabel value="meter" control={<Radio />} label="Meter" />
+                        <FormControlLabel value="inches" control={<Radio />} label="Inches" />
+                        <FormControlLabel value="centimeters" control={<Radio />} label="Centimeters" />
+                    </RadioGroup>
+                </FormControl>
+
+                <Grid container spacing={3} style={{ padding: 50, paddingTop: 10 }}>
+
+                    <Grid item xs={12} sm={4}>
+                        <TextField
+                            required
+                            id="height"
+                            name="height"
+                            label="Height"
+                            fullWidth
+                            autoComplete="Height"
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <TextField
+                            id="width"
+                            name="width"
+                            label="Width"
+                            fullWidth
+                            autoComplete="width"
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <TextField
+                            required
+                            id="length"
+                            name="length"
+                            label="Length"
+                            fullWidth
+                            autoComplete="Length"
+                        />
+                    </Grid>
+                </Grid>
+                
+            </form>
+        </CardContent>
+    )
+}
+
+export default ProductDimensions
